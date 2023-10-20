facd77874ecf881c92746fd6570cb6e24d92bee9
Diogo Landau <diogo.hewitt.landau@hotmail.com>
2023-10-20 15:51:09 +0200

    Add timestamp field to gt notification

--- database/ddl/notification_ground_truth.sql
+++ database/ddl/notification_ground_truth.sql
@@ -3,5 +3,6 @@ DROP TABLE demo.notification_ground_truth;
 CREATE TABLE demo.notification_ground_truth (
     experiment_id TEXT,
     measurement_id TEXT, 
+    insert_timestamp TIMESTAMP DEFAULT now(),
     PRIMARY KEY(experiment_id, measurement_id)
 );
